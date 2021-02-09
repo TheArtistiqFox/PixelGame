94fd89c25727c4db8a0a812fc3ce504442b781cc
matt danner <matt.danner@gmail.com>
2021-02-09 15:35:09 -0800

    demo animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,18 +9,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed = 1f;
     [SerializeField] private float _jumpSpeed = 1f;
     [SerializeField] private LayerMask _ground;
-    
+
     private PlayerControls _playerControls;
 
     private Rigidbody2D _rigidbody2D;
     private Collider2D _collider2D;
+    private Animator _animator;
     
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _collider2D = GetComponent<Collider2D>();
+        _animator = GetComponent<Animator>();
         
         _playerControls = new PlayerControls();
+    }
+
+    private int Add(int a, int b)
+    {
+        int c = a + b;
+        return c;
     }
 
     private void Start()
@@ -66,5 +74,15 @@ public class PlayerController : MonoBehaviour
         Vector3 currentPosition = transform.position;
         currentPosition.x += movementInput * _speed * Time.deltaTime;
         transform.position = currentPosition;
+        
+        _animator.SetFloat("Speed", Mathf.Abs(movementInput));
+        _animator.SetBool("IsJumping", !IsGrounded());
+
+        if (movementInput != 0)
+        {
+            Vector3 currentScale = transform.localScale;
+            currentScale.x = movementInput > 0 ? 1 : -1;
+            transform.localScale = currentScale;
+        }
     }
 }
